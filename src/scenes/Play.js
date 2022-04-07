b5f379188708284ce2efc9f0996620337327cf7e
Matthew Meacham <mightymatthewme@gmail.com>
2022-04-07 09:17:44 -0700

    added UI stuff

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,5 +5,12 @@ class Play extends Phaser.Scene {
 
     create() {
         this.add.text(20, 20, "Rocket Patrol Play");
+        //green UI background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize*2, 0x00FF00).setOrgin(0,0);
+        //White borders
+        this.add.rectangle(0, 0, gmae.config.width, borderUISize, 0xFFFFFF).setOrgin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrgin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
     }
 }
